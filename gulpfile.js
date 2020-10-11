991d6005b1a1f4f8e320ee9b8bd0b392cc90c828
alexkeleberda <alexkeleberda@gmail.com>
2020-10-11 22:02:36 +0300

    Продолжаю интенсив Барбершоп. Начал стилизацию первой страницы

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,3 @@
-'use strict';
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-sass.compiler = require('node-sass');
-
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
-
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
-
-var sourcemaps = require('gulp-sourcemaps');
-
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'));
+gulp.task('hello', function() {
+  console.log('Hello Zell');
 });
